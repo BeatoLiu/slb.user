faf033bf95fc6cbd43d277d0c76803e7d83b6ca8
BeatoLiu <lzf280069431@163.com>
2022-01-05 16:45:14 +0800

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import Routes from './routes'
-import { ConfigProvider } from 'react-vant'
-import './App.less'
+import React from "react";
+import Routes from "./routes";
+import { ConfigProvider } from "react-vant";
+import "./App.less";
 
 const themeVars = {
-    '--rv-button-primary-background-color': '#39b9b9',
-    '--rv-button-primary-border-color': '#39b9b9'
-}
-
+	"--rv-button-primary-background-color": "#39b9b9",
+	"--rv-button-primary-border-color": "#39b9b9"
+};
 const App: React.FC = () => {
-    return (
-        <div className="app">
-            <ConfigProvider themeVars={themeVars}>
-                <Routes/>
-            </ConfigProvider>
-        </div>
-    );
-}
-
+	return (
+		<div className="app">
+			<ConfigProvider themeVars={themeVars}>
+				<Routes />
+			</ConfigProvider>
+		</div>
+	);
+};
 export default App;
